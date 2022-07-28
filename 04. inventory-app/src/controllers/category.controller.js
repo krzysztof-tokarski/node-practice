93aa500ac7325f0bd48d8f7785144b6c68577caf
krzysztof.tokarski <krzysztof.tokarski96@gmail.com>
2022-07-28 22:38:40 +0200

    fix: debug request hanging

--- 04. inventory-app/src/controllers/category.controller.js
+++ 04. inventory-app/src/controllers/category.controller.js
@@ -1,36 +1,36 @@
 const Category = require('../models/category.model');
 
-exports.GETlistOfAllCategories = (request, response, next) => {
-  Category.find({})
-  .sort({name: 1})
-  .exec((error, results) => {
-    if (error) return next(error);
+// exports.GETlistOfAllCategories = (request, response, next) => {
+//   Category.find({})
+//   .sort({name: 1})
+//   .exec((error, results) => {
+//     if (error) return next(error);
 
-    console.log(response);
-    response.render('', {
-      // categoriesList: results.categor
-    })
-  })
-}
+//     console.log(response);
+//     response.render('', {
+//       // categoriesList: results.categor
+//     })
+//   })
+// }
 
-exports.GETcategoryDetailPage = (request, respons, next) => {
-  // Category.findById(request.params.id).exec((error, response) => {
-  //   if (error) return next(error);
-  //   if (response.category === null) {
-  //     const error404 = new Error('Could not find a category with the provided id.');
-  //     error404.status = 404;
-  //     return next(error404);
-  //   }
+// exports.GETcategoryDetailPage = (request, respons, next) => {
+//   // Category.findById(request.params.id).exec((error, response) => {
+//   //   if (error) return next(error);
+//   //   if (response.category === null) {
+//   //     const error404 = new Error('Could not find a category with the provided id.');
+//   //     error404.status = 404;
+//   //     return next(error404);
+//   //   }
 
-  //   console.log(response);
-  //   response.render('', {
-  //     category: "",
-  //   })
-  // })
-}
+//   //   console.log(response);
+//   //   response.render('', {
+//   //     category: "",
+//   //   })
+//   // })
+// }
 
 exports.GETcreateCategoryForm = (request, response, next) => {
-  response.render('category-form');
+  response.render('category_form');
 }
 
 exports.POSTcreateCategoryForm = (request, response) => {
